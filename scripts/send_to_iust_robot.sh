b0327ead0a99606e8e763f488be888cfb15b8885
aligolestaneh <aliigolestaneh@gmail.com>
2023-01-24 14:40:29 +0330

    Modify main CMakeLists

---
+++ scripts/send_to_iust_robot.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Author: aligolestaneh
+# Date: 01/24/2023
+# This shell script is modified based on MIT corresponding script to send software to real robot.
+# Unlike MIT, we can select iust_cheetah's ip address and controller by options.
+# wifi ip is the default address and the iust_ctrl is the default controller if don't specify it.
+
+echo "[USAGE] sh send_to_iust_cheetah.sh [Controller_path/executable] [wifi|wire]"
+echo "[EXAMPLE] sh send_to_iust_cheetah.sh IUST_Controller/iust_ctrl wifi\n"
+## Set robot's ip
+wifiip=10.61.6.124
+wireip=10.0.0.21
+name=robot
+
+## Create a prepare dir robot-software
+set -e
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(pwd)"
+cd ${DIR}/../../mc-build/
+rm -rf robot-software
+mkdir robot-software
+mkdir robot-software/build
+#cp common/test-common robot-software/build
+if [ -z $1 ]
+then
+  cp ../mc-build/user/IUST_Controller/iust_ctrl robot-software/build
+else
+  cp ../mc-build/user/$1 robot-software/build
+fi
+find . -name \*.so* -exec cp {} ./robot-software/build \;
+cp ../scripts/run_mc* ./robot-software/build
+cp ../scripts/setup_network_mc.py ./robot-software/build
+cp ../scripts/run_mc_debug.sh ./robot-software/build
+cp ../scripts/config_network_lcm.sh ./robot-software
+cp -r ../robot robot-software
+cp -r ../config robot-software
+
+## Send dir to real robot's computer
+if [ -n "$2" ]
+then
+  if [ "$2" = "wifi" ]
+  then
+    echo "[INFO] Using Wifi connection ip address.\nSending ..."
+    scp -rq robot-software $name@$wifiip:~/
+  else
+    echo "[INFO] Using wire connection ip address.\nSending ..."
+    scp -rq robot-software $name@$wireip:~/
+  fi
+else
+    echo "[INFO] No connection type specified, using wifi connection as default.\nSending ..."
+    scp -rq robot-software $name@$wifiip:~/
+fi
+
+## Check result
+if [ $? -eq 0 ]
+then
+  echo "[INFO] Finish software sending successfully."
+else
+  echo "[ERROR] Something wrong, sending failed."
+fi
